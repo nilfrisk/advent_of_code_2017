3edbd744d7a7c2a5a57d02ce5e6bba94bb863c8c
nilfrisk <robert.c.bennett@gmail.com>
2017-12-10 18:52:35 -0600

    Day 7: Day 8: Day 9:

--- advent_of_code/advent_of_code.cpp
+++ advent_of_code/advent_of_code.cpp
@@ -6,6 +6,222 @@
 #include <algorithm>
 
 
+size_t advent_of_code::week_7::get_tower_weight(const ProgramTree& program_tree){
+
+    size_t weight(program_tree.weight);
+
+    for(const auto& top_program_tree : program_tree.program_trees){
+        weight += get_tower_weight(top_program_tree);
+    }
+
+    return(weight);
+}
+
+size_t advent_of_code::week_7::get_weight_required_to_balance_tower(const advent_of_code::week_7::ProgramTree& program_tree){
+
+    //NOTE: weight_required_to_balance_tower must be !0 to be a valid number
+    size_t weight_required_to_balance_tower(0);
+
+    for(const auto& top_program_tree : program_tree.program_trees){
+        weight_required_to_balance_tower = get_weight_required_to_balance_tower(top_program_tree);
+        if(0 != weight_required_to_balance_tower){ break; }
+    }
+    if(0 != weight_required_to_balance_tower){ return(weight_required_to_balance_tower); }
+
+    if(program_tree.program_trees.empty()){ assert(!program_tree.holding_disc); return(weight_required_to_balance_tower); }
+
+    assert(program_tree.holding_disc);
+    assert(2 < program_tree.program_trees.size());
+
+    //Are the weights balanced?
+    std::vector<size_t> first_weights;
+    std::vector<size_t> other_weights;
+    const auto first_weight(get_tower_weight(program_tree.program_trees[0]));
+    first_weights.push_back(0);
+
+    for(size_t index(1); program_tree.program_trees.size() != index; ++index){
+        auto tower_weight(get_tower_weight(program_tree.program_trees[index]));
+        if(first_weight == tower_weight){
+            first_weights.push_back(index);
+            continue;
+        }
+
+        other_weights.push_back(index);
+    }
+    if(other_weights.empty()){ return(weight_required_to_balance_tower); }
+
+    //Which weight is not balanced (NOTE: We are told there is only one in the entire set)
+
+    if(1 == first_weights.size()){
+        //index 0 is the unbalanced
+        assert(1 == first_weights.size());
+        assert(!other_weights.empty());
+
+        const auto& program_tree_balanced = program_tree.program_trees[other_weights[0]];
+        const auto& program_tree_unbalanced = program_tree.program_trees[first_weights[0]];
+
+        weight_required_to_balance_tower = get_tower_weight(program_tree_balanced) - (get_tower_weight(program_tree_unbalanced) - program_tree_unbalanced.weight);
+
+
+    } else{
+        assert(1 == other_weights.size());
+        assert(!first_weights.empty());
+
+        const auto& program_tree_balanced = program_tree.program_trees[first_weights[0]];
+        const auto& program_tree_unbalanced = program_tree.program_trees[other_weights[0]];
+
+        weight_required_to_balance_tower = get_tower_weight(program_tree_balanced) - (get_tower_weight(program_tree_unbalanced) - program_tree_unbalanced.weight);
+
+    }
+    assert(0 != weight_required_to_balance_tower);
+    if(0 != weight_required_to_balance_tower){ return(weight_required_to_balance_tower); }
+
+    return(weight_required_to_balance_tower);
+}
+std::vector<std::string> advent_of_code::week_8::import_data(){
+
+    auto data = advent_of_code::get_data("..//input.txt");
+    assert(!data.empty());
+
+    auto strings = std::tokenize(data, "\n");
+    assert(!strings.empty());
+
+    data.clear();
+
+    return(strings);
+
+}
+namespace advent_of_code{
+    namespace week_7{
+        struct NameAndWeight{
+            std::string name;
+            size_t weight = 0;
+
+            void Clear(){
+                name.clear();
+                weight = 0;
+            }
+        };
+        NameAndWeight parse_name_and_weight(std::stringstream& string_stream, const std::string& name_and_weight_string){
+
+            assert(!name_and_weight_string.empty());
+
+            auto tokens = std::tokenize(name_and_weight_string, "(");
+            assert(2 == tokens.size());
+
+            NameAndWeight name_and_weight;
+
+            name_and_weight.name = tokens[0];
+            assert(!name_and_weight.name.empty());
+
+            auto weight_string(tokens[1]);
+            assert(!weight_string.empty() && 1 < weight_string.size() && ')' == weight_string[weight_string.size() - 1]);
+
+            //remove the trailing )
+            weight_string.resize(weight_string.size() - 1);
+
+            string_stream << weight_string;
+            string_stream >> name_and_weight.weight;
+            assert(!string_stream.fail());
+
+            return(name_and_weight);
+        }
+        std::vector<std::string> parse_tops(const std::string& tops_string){
+            std::vector<std::string> tops;
+            if(tops_string.empty()){ return(tops); }
+
+            tops = std::tokenize(tops_string, ",");
+            assert(!tops.empty());
+
+            return(tops);
+        }
+    }
+}
+
+bool advent_of_code::week_7::process_program_trees(ProgramTree& this_program_tree, std::vector<ProgramTree>& program_trees){
+
+    bool found(false);
+
+    for(auto& program_tree : this_program_tree.program_trees){
+        found = process_program_trees(program_tree, program_trees);
+        if(found){ break; }
+    }
+    if(found){ return(found); }
+
+    //add any existinig program_trees to program_tree 
+    for(auto index(0); program_trees.size() != index; ++index){
+
+        const auto& that_program_tree(program_trees[index]);
+
+        if(0 == this_program_tree.name.compare(that_program_tree.name)){ continue; }
+
+        //check if it_program_tree is a top of this_program_tree
+        for(const auto& top : this_program_tree.tops){
+            if(0 != top.compare(that_program_tree.name)){ continue; }
+
+            found = true;
+
+            this_program_tree.program_trees.push_back(that_program_tree);
+            program_trees.erase(program_trees.begin() + index);
+
+            //reset iterator
+            index = -1;
+            break;
+        }
+        if(found){ break; }
+    }
+    if(found){ return(found); }
+
+    return(found);
+}
+std::vector<advent_of_code::week_7::Program> advent_of_code::week_7::import_data(){
+
+    auto data = advent_of_code::get_data("..//day_7_input.txt");
+    assert(!data.empty());
+
+    auto program_strings = std::tokenize(data, "\n");
+    assert(!program_strings.empty());
+
+    data.clear();
+
+    NameAndWeight name_and_weight;
+    Program program;
+    std::vector<Program> programs;
+    std::stringstream string_stream;
+
+    std::for_each(program_strings.cbegin(), program_strings.cend(), [&programs, &program, &name_and_weight, &string_stream](std::string program_string){
+        assert(!program_string.empty());
+
+        //Scrap the spaces     
+        auto end = std::remove(program_string.begin(), program_string.end(), ' ');
+
+        program_string.resize(program_string.size() - (program_string.end() - end));
+
+        //Get holding_disc
+        auto tokens = std::tokenize(program_string, "->");
+        assert(!tokens.empty());
+
+        program.holding_disc = 1 != tokens.size();
+
+        //Get name and weight
+        name_and_weight = parse_name_and_weight(string_stream, tokens[0]);
+        program.name = name_and_weight.name;
+        program.weight = name_and_weight.weight;
+
+        if(program.holding_disc){
+            program.tops = parse_tops(tokens[1]);
+        }
+
+        programs.push_back(program);
+
+        //reset vars
+        std::reset(string_stream);
+        program.Clear();
+        name_and_weight.Clear();
+    });
+    return(programs);
+
+}
 std::vector<size_t> advent_of_code::week_6::import_data(){
     auto data = advent_of_code::get_data("..//day_6_part_1_input.txt");
     assert(!data.empty());
@@ -437,4 +653,266 @@ std::vector<std::string> std::tokenize(std::string value, const std::string& del
         token = strtok(nullptr, delims.c_str());
     }
     return(tokens);
+}
+
+
+
+void advent_of_code::week_8::JumpInstruction::ProcessToken0(const std::string& token){
+
+    //i.e. b inc 580
+    auto tokens = std::tokenize(token, " ");
+    assert(3 == tokens.size());
+
+    //register_to_modify
+    this->modify.register_to_modify = tokens[0];
+
+    //modify.direction
+    if(0 == tokens[1].compare("inc")){
+        this->modify.direction = Modify::Direction::increase;
+    } else if(0 == tokens[1].compare("dec")){
+        this->modify.direction = Modify::Direction::decrease;
+    } else{
+        assert(false);
+    }
+
+    //modify.amount
+    std::reset(m_string_stream);
+    m_string_stream << tokens[2];
+    m_string_stream >> this->modify.amount;
+    assert(!m_string_stream.fail());
+}
+void advent_of_code::week_8::JumpInstruction::ProcessCondition(const std::string& condition_string){
+
+    //i.e. bkd > -1
+    auto tokens = std::tokenize(condition_string, " ");
+    assert(3 == tokens.size());
+
+    //condition.operation
+    condition.register_to_compare = tokens[0];
+
+    //condition.operation
+    if(0 == tokens[1].compare("==")){
+        this->condition.operation = Condition::Operation::equal_to;
+    } else if(0 == tokens[1].compare(">")){
+        this->condition.operation = Condition::Operation::greater_than;
+    } else if(0 == tokens[1].compare(">=")){
+        this->condition.operation = Condition::Operation::greater_than_or_equal_to;
+    } else if(0 == tokens[1].compare("<")){
+        this->condition.operation = Condition::Operation::less_than;
+    } else if(0 == tokens[1].compare("<=")){
+        this->condition.operation = Condition::Operation::less_than_or_equal_to;
+    } else if(0 == tokens[1].compare("!=")){
+        this->condition.operation = Condition::Operation::not_equal_to;
+    } else{
+        assert(false);
+    }
+
+    //condition.amount
+    std::reset(m_string_stream);
+    m_string_stream << tokens[2];
+    m_string_stream >> this->condition.amount;
+    assert(!m_string_stream.fail());
+}
+advent_of_code::week_8::JumpInstruction::JumpInstruction(const std::string& jump_instruction){
+    //parse jump_instruction string
+
+    //i.e. b inc 580 if bkd > -1
+    auto pos = jump_instruction.find(if_string);
+    assert(jump_instruction.npos != pos);
+
+    this->ProcessToken0(jump_instruction.substr(0, pos));
+    this->ProcessCondition(jump_instruction.substr(pos + if_string.size()));
+
+    //auto tokens = std::tokenize(jump_instruction, "if");
+    //assert(2 == tokens.size());
+    //
+    //this->ProcessToken0(tokens[0]);
+    //this->ProcessCondition(tokens[1]);
+}
+bool advent_of_code::week_8::get_process_instruction(const int register_value, const JumpInstruction::Condition::Operation operation, const int condition_amount){
+    bool process_instruction(false);
+    switch(operation){
+    case JumpInstruction::Condition::Operation::equal_to:
+        process_instruction = register_value == condition_amount;
+        break;
+    case JumpInstruction::Condition::Operation::greater_than:
+        process_instruction = register_value > condition_amount;
+        break;
+    case JumpInstruction::Condition::Operation::greater_than_or_equal_to:
+        process_instruction = register_value >= condition_amount;
+        break;
+    case JumpInstruction::Condition::Operation::less_than:
+        process_instruction = register_value < condition_amount;
+        break;
+    case JumpInstruction::Condition::Operation::less_than_or_equal_to:
+        process_instruction = register_value <= condition_amount;
+        break;
+    case JumpInstruction::Condition::Operation::not_equal_to:
+        process_instruction = register_value != condition_amount;
+        break;
+    default:
+        assert(false);
+        break;
+    };
+    return(process_instruction);
+}
+void advent_of_code::week_8::process_jump_instruction(std::vector<Register>& registers, const JumpInstruction& jump_instruction){
+
+    //Get the register to compare
+    auto it_register_to_compare = std::find_if(registers.cbegin(), registers.cend(), [&jump_instruction](const Register& the_register){
+        return(0 == jump_instruction.condition.register_to_compare.compare(the_register.name));
+    });
+    if(it_register_to_compare == registers.cend()){
+        //initialize register
+        registers.push_back(jump_instruction.condition.register_to_compare);
+        it_register_to_compare = registers.end() - 1;
+    }
+    assert(registers.cend() != it_register_to_compare);
+
+    bool process_instruction(get_process_instruction(it_register_to_compare->value, jump_instruction.condition.operation, jump_instruction.condition.amount));
+    if(!process_instruction){ return; }
+
+    //Get the register to modify
+    auto it_register_to_modify = std::find_if(registers.begin(), registers.end(), [&jump_instruction](const Register& the_register){
+        return(0 == jump_instruction.modify.register_to_modify.compare(the_register.name));
+    });
+    if(it_register_to_modify == registers.cend()){
+        //initialize register
+        registers.push_back(jump_instruction.modify.register_to_modify);
+        it_register_to_modify = registers.end() - 1;
+    }
+    assert(registers.cend() != it_register_to_modify);
+
+
+    switch(jump_instruction.modify.direction){
+    case JumpInstruction::Modify::Direction::decrease:
+        it_register_to_modify->value -= jump_instruction.modify.amount;
+        break;
+    case JumpInstruction::Modify::Direction::increase:
+        it_register_to_modify->value += jump_instruction.modify.amount;
+        break;
+    default:
+        assert(false);
+        break;
+    }
+}
+
+void advent_of_code::week_8::process_jump_instruction(int& highest_value_held, std::vector<Register>& registers, const JumpInstruction& jump_instruction){
+
+    //Get the register to compare
+    auto it_register_to_compare = std::find_if(registers.cbegin(), registers.cend(), [&jump_instruction](const Register& the_register){
+        return(0 == jump_instruction.condition.register_to_compare.compare(the_register.name));
+    });
+    if(it_register_to_compare == registers.cend()){
+        //initialize register
+        registers.push_back(jump_instruction.condition.register_to_compare);
+        it_register_to_compare = registers.end() - 1;
+    }
+    assert(registers.cend() != it_register_to_compare);
+
+    bool process_instruction(get_process_instruction(it_register_to_compare->value, jump_instruction.condition.operation, jump_instruction.condition.amount));
+    if(!process_instruction){ return; }
+
+    //Get the register to modify
+    auto it_register_to_modify = std::find_if(registers.begin(), registers.end(), [&jump_instruction](const Register& the_register){
+        return(0 == jump_instruction.modify.register_to_modify.compare(the_register.name));
+    });
+    if(it_register_to_modify == registers.cend()){
+        //initialize register
+        registers.push_back(jump_instruction.modify.register_to_modify);
+        it_register_to_modify = registers.end() - 1;
+    }
+    assert(registers.cend() != it_register_to_modify);
+
+
+    switch(jump_instruction.modify.direction){
+    case JumpInstruction::Modify::Direction::decrease:
+        it_register_to_modify->value -= jump_instruction.modify.amount;
+        break;
+    case JumpInstruction::Modify::Direction::increase:
+        it_register_to_modify->value += jump_instruction.modify.amount;
+        break;
+    default:
+        assert(false);
+        break;
+    }
+
+    if(highest_value_held < std::abs(it_register_to_modify->value)){ highest_value_held = std::abs(it_register_to_modify->value); }
+}
+
+size_t advent_of_code::week_9::get_garbage_count(const advent_of_code::week_9::Group& group){
+
+    size_t garbage_count(0);
+
+    for(const auto& child_group : group.groups){
+        garbage_count += advent_of_code::week_9::get_garbage_count(child_group);
+    }
+    for(const auto& garbage : group.garbage){
+        garbage_count += garbage.size();
+    }
+
+    return(garbage_count);
+}
+size_t advent_of_code::week_9::get_score(const advent_of_code::week_9::Group& group){
+
+    size_t score(0);
+
+    for(const auto& child_group : group.groups){
+        score += advent_of_code::week_9::get_score(child_group);
+    }
+    score += group.score;
+
+    return(score);
+}
+
+advent_of_code::week_9::Group advent_of_code::week_9::on_group(std::stringstream& string_stream, const size_t score){
+    //group{}
+
+    Group group;
+    group.score = score;
+
+    std::string line;
+    for(auto current_char(string_stream.get()); '}' != current_char; current_char = string_stream.get()){
+
+        switch(current_char){
+        case '{':
+            group.groups.push_back(on_group(string_stream, score + 1));
+            break;
+        case '<':
+            group.garbage.push_back(on_garbage(string_stream));
+            break;
+        case ',':
+            //new item
+            break;
+        default:
+            assert(false);
+            break;
+        }
+    }
+
+    return(group);
+}
+
+std::string advent_of_code::week_9::on_garbage(std::stringstream& string_stream){
+    //garbage<>
+    //within garbage canceled ! (ignore the char after !)
+
+    std::stringstream local_string_stream;
+
+    for(auto current_char(string_stream.get()); '>' != current_char; current_char = string_stream.get()){
+
+        switch(current_char){
+        case '!':
+            //ignore the char after !
+            //local_string_stream << current_char;
+            current_char = string_stream.get();
+            break;
+        default:
+            local_string_stream << static_cast<char>(current_char);
+            break;
+        }
+    }
+
+    std::string garbage(local_string_stream.str());
+    return(garbage);
 }
--- advent_of_code/advent_of_code.h
+++ advent_of_code/advent_of_code.h
@@ -5,20 +5,121 @@
 #include <vector>
 #include <sstream>
 
+
 namespace std{
     void reset(std::stringstream& string_stream);
     std::vector<std::string> tokenize(std::string value, const std::string& delims);
 }
 namespace advent_of_code{
+
     std::string get_data(const char* file_path);
-    
-    namespace week_5{
-        std::vector<int> import_data();
+
+    namespace week_9{
+
+        struct Group{
+            std::vector<Group> groups;
+            std::vector<std::string> garbage;
+            size_t score = 0;
+        };
+        Group on_group(std::stringstream& string_stream, const size_t score);
+        std::string on_garbage(std::stringstream& string_stream);
+
+        size_t get_score(const Group& group);
+        size_t get_garbage_count(const Group& group);
+
+    }
+
+    namespace week_8{
+        std::vector<std::string> import_data();
+
+        class JumpInstruction{
+        public:
+            JumpInstruction(const std::string& jump_instruction);
+
+        public:
+
+            struct Modify{
+                enum class Direction{ unknown, increase, decrease };
+
+                std::string register_to_modify;
+                Direction direction = Direction::unknown;
+                int amount = 0;
+        }modify;
+
+            struct Condition{
+                enum class Operation{
+                    unknown,
+                    greater_than,
+                    less_than,
+                    greater_than_or_equal_to,
+                    less_than_or_equal_to,
+                    equal_to,
+                    not_equal_to
+                };
+                std::string register_to_compare;
+                Operation operation = Operation::unknown;
+                int amount = 0;
+            }condition;
+        private:
+            void ProcessToken0(const std::string& token);
+            void ProcessCondition(const std::string& condition_string);
+
+            std::stringstream m_string_stream;
+
+            const std::string if_string = " if ";
+    };
+
+        struct Register{
+            Register(const std::string& name) :
+                name(name){
+            }
+            std::string name;
+            int value = 0;
+        };
+        void process_jump_instruction(std::vector<Register>& registers, const JumpInstruction& jump_instruction);
+        void process_jump_instruction(int& highest_value_held, std::vector<Register>& registers, const JumpInstruction& jump_instruction);
+        bool get_process_instruction(const int register_value, const JumpInstruction::Condition::Operation operation, const int condition_amount);
+
+    }
+    namespace week_7{
+        struct Program{
+
+#ifdef TEST_DATA
+            Program(const std::string& name = std::string(), const size_t weight = 0, const bool holding_disc = false, const std::vector<std::string>& tops = std::vector<std::string>()):
+                name(name), weight(weight), holding_disc(holding_disc), tops(tops)
+            {}
+#endif
+
+            std::string name;
+            size_t weight = 0;
+            bool holding_disc = false;
+            std::vector<std::string> tops;
+
+            void Clear(){
+                name.clear();
+                weight = 0;
+                holding_disc = false;
+                tops.clear();
+            }
+        };
+        std::vector<Program> import_data();
+
+        struct ProgramTree :
+            public Program{
+            std::vector<ProgramTree> program_trees;
+        };
+        void copy(advent_of_code::week_7::Program& this_program, const advent_of_code::week_7::Program& that_program);
+        bool process_program_trees(ProgramTree& this_program_tree, std::vector<ProgramTree>& program_trees);
+
+        size_t get_weight_required_to_balance_tower(const ProgramTree& program_tree);
+        size_t get_tower_weight(const ProgramTree& program_tree);
     }
     namespace week_6{
         std::vector<size_t> import_data();
     }
-
+    namespace week_5{
+        std::vector<int> import_data();
+    }
     size_t get_manhattan_distance(const std::pair<int, int> this_item, const std::pair<int, int> that_item);
 
     namespace spiral_memory{
@@ -31,3 +132,7 @@ inline void std::reset(std::stringstream& string_stream){
     string_stream.clear();
     string_stream.str("");
 }
+inline void advent_of_code::week_7::copy(advent_of_code::week_7::Program& this_program, const advent_of_code::week_7::Program& that_program){
+    this_program = that_program;
+}
+
